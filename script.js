4713bbe22c56811cdf878aabbdbd2cc8626ea1fb
ppruvost <patrick.pruvost50@gmail.com>
2025-12-12 12:26:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ function measure() {
     soundBar.style.width = percent + "%";
 
     // Emoji selon le niveau
-    if (displayDb < 30) emoji.textContent = "😊";
+    if (displayDb < 40) emoji.textContent = "😊";
     else if (displayDb < 60) emoji.textContent = "😐";
     else emoji.textContent = "😣";
 
